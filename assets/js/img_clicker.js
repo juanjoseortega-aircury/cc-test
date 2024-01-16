a3e4a5cd7a5f2d1183bb77c60565162f76938164
Juan Jose Ortega <juanjo.ortega@aircury.com>
2024-01-16 09:15:54 +0100

    #58 | Add popup functionality at clicking images

---
+++ assets/js/img_clicker.js
@@ -0,0 +1,23 @@
+$(function () {
+    "use strict";
+
+    $(".containerTeam .imgMembers").click(function () {
+        var $src = $(this).attr("src"); 
+        var $description = $(this).siblings("#desc").text();
+        var $name= $(this).siblings("h4").text();
+        var $job= $(this).siblings(".job").text();
+        document.getElementById("descPopUp").innerHTML=$description;
+        document.getElementById("namePopUp").innerHTML=$name;
+        document.getElementById("jobPopUp").innerHTML=$job;
+        $(".showPopUp").fadeIn();
+        $(".img-show img").attr("src", $src);
+        $("html").addClass("no-scroll");
+    });
+
+    
+    $("span, .overlay").click(function () {
+        $(".showPopUp").fadeOut();
+        $("html").removeClass("no-scroll");
+    });
+    
+});
