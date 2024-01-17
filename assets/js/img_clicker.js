9c8b04d0982160a84ccc839cf53b69ce2095fd6d
Juan Jose Ortega <juanjo.ortega@aircury.com>
2024-01-17 10:46:26 +0100

    #58 | Fix indentations, spaces and refactor scss

--- assets/js/img_clicker.js
+++ assets/js/img_clicker.js
@@ -4,11 +4,11 @@ $(function () {
     $(".containerTeam .imgMembers").click(function () {
         var $src = $(this).attr("src"); 
         var $description = $(this).siblings("#desc").text();
-        var $name= $(this).siblings("h4").text();
-        var $job= $(this).siblings(".job").text();
-        document.getElementById("descPopUp").innerHTML=$description;
-        document.getElementById("namePopUp").innerHTML=$name;
-        document.getElementById("jobPopUp").innerHTML=$job;
+        var $name = $(this).siblings("h4").text();
+        var $job = $(this).siblings(".job").text();
+        document.getElementById("descPopUp").innerHTML = $description;
+        document.getElementById("namePopUp").innerHTML = $name;
+        document.getElementById("jobPopUp").innerHTML = $job;
         $(".showPopUp").fadeIn();
         $(".img-show img").attr("src", $src);
         $("html").addClass("no-scroll");
