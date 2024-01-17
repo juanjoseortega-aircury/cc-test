9a97c122a15b80d4fe6088a827f9227bb640ede6
Juan Jose Ortega <juanjo.ortega@aircury.com>
2024-01-17 11:15:48 +0100

    #58 | Fix vulnerability from js

--- assets/js/img_clicker.js
+++ assets/js/img_clicker.js
@@ -6,9 +6,9 @@ $(function () {
         var $description = $(this).siblings("#desc").text();
         var $name = $(this).siblings("h4").text();
         var $job = $(this).siblings(".job").text();
-        document.getElementById("descPopUp").innerHTML = $description;
-        document.getElementById("namePopUp").innerHTML = $name;
-        document.getElementById("jobPopUp").innerHTML = $job;
+        document.getElementById("descPopUp").textContent = $description;
+        document.getElementById("namePopUp").textContent = $name;
+        document.getElementById("jobPopUp").textContent = $job;
         $(".showPopUp").fadeIn();
         $(".img-show img").attr("src", $src);
         $("html").addClass("no-scroll");
