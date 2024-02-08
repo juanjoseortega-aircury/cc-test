0a68a9ea7bda7d86b68dd30af655e9fbf4152656
Juan Jose Ortega <juanjo.ortega@aircury.com>
2024-02-08 10:37:35 +0100

    #72 | Move English language content and fix redirect to each about language page

--- assets/js/_js/default/contact_form.js
+++ assets/js/_js/default/contact_form.js
@@ -3,13 +3,18 @@ function sendForm(alertText) {
     var option = document.getElementById("select-form").value;
     var name = document.getElementById("name").value;
     document.getElementById("form-subject").value = option + "-" + name;
-    document.getElementById("form-redirect").value = 'http://'+window.location.host;
-    $('#contact-form input, #contact-form textarea ').each(function () {
-        if ($(this).val() === '') {
+    var pathname = window.location.pathname;
+    var link = window.location.host;
+    if (!pathname.includes("/en/")) {
+        link = link + pathname.slice(0, pathname.lastIndexOf("/")) + "/about";
+    }
+    document.getElementById("form-redirect").value = "http://" + link;
+    $("#contact-form input, #contact-form textarea ").each(function () {
+        if ($(this).val() === "") {
             formInvalid = true;
         }
     });
-    if ($('#select-form :selected').val()=== ''){
+    if ($("#select-form :selected").val()=== ""){
         formInvalid = true;
     }
     if (!formInvalid) {
