f39e55240c995bc81502e6dabd17e365310b132e
Juan Jose Ortega <juanjo.ortega@aircury.com>
2024-02-09 12:55:46 +0100

    #72 | Remove js validators

--- assets/js/_js/default/contact_form.js
+++ assets/js/_js/default/contact_form.js
@@ -1,5 +1,4 @@
 function sendForm(alertText) {
-    var formInvalid = false;
     var option = document.getElementById("select-form").value;
     var name = document.getElementById("name").value;
     document.getElementById("form-subject").value = option + "-" + name;
@@ -9,17 +8,7 @@ function sendForm(alertText) {
         link = link + pathname.slice(0, pathname.lastIndexOf("/")) + "/about";
     }
     document.getElementById("form-redirect").value = "http://" + link;
-    $("#contact-form input, #contact-form textarea ").each(function () {
-        if ($(this).val() === "") {
-            formInvalid = true;
-        }
-    });
-    if ($("#select-form :selected").val()=== ""){
-        formInvalid = true;
-    }
-    if (!formInvalid) {
-        alert(alertText);
-    }
+    alert(alertText);
 };
 function hcaptchaCallback() {
     $("#contact-form button").removeAttr("disabled");
